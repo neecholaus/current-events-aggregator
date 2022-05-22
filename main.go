6338602f11dcf16049efe9646fa311aa8440cd9b
neecholaus <thisisnickadam@gmail.com>
2022-05-22 12:38:51 -0700

    headlines now stored in created at scoped files

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -13,6 +15,8 @@ func main() {
 		"associatepress": "https://apnews.com",
 		"theeconomist":   "https://www.economist.com",
 	}
+
+	extractions := []Extraction{}
 
 	for sourceName, sourceHost := range pulls {
 		// Pull html
@@ -24,6 +28,7 @@ func main() {
 
 		defer res.Body.Close()
 
+		// Isolate response body
 		body, err := ioutil.ReadAll(res.Body)
 		if err != nil {
 			fmt.Println(err.Error())
@@ -36,19 +41,37 @@ func main() {
 		}
 
 		newsPull.ExtractHeadlinesFromHTML(string(body))
-
 		if newsPull.LastError != "" {
 			fmt.Println("Failed to extract headlines.")
 			fmt.Println(newsPull.LastError)
 			return
 		}
 
-		for i := 0; i < len(newsPull.Headlines); i++ {
-			fmt.Println(fmt.Sprintf("%s\t%s\t%s",
-				newsPull.CreatedAt.UTC().Format(time.UnixDate),
-				newsPull.SourceName,
-				newsPull.Headlines[i]),
-			)
+		extractions = append(extractions, newsPull)
+	}
+
+	storeExtractions(extractions)
+}
+
+func storeExtractions(extractions []Extraction) {
+	// Create storage for current pull
+	filepath := fmt.Sprintf("./headlines/%s.tsv", extractions[0].CreatedAt.Format(time.UnixDate))
+	file, err := os.OpenFile(filepath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, os.FileMode(0640))
+	if err != nil {
+		panic(err)
+	}
+
+	// Create slice of formatted lines
+	var entries []string
+	for _, extraction := range extractions {
+		for _, headline := range extraction.Headlines {
+			entries = append(entries, fmt.Sprintf("%s\t%s\t%s\n",
+				extraction.CreatedAt.Format(time.UnixDate),
+				extraction.SourceName,
+				headline,
+			))
 		}
 	}
+
+	file.WriteString(strings.Join(entries, ""))
 }
